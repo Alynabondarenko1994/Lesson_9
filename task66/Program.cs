0237b0e13275207d0930ad572eee308acbf568fa
1 <bondarenko-1994alenka@mail.ru>
2022-06-29 21:47:33 +0300

    Lesson 9

---
+++ task66/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetNumber(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Введено ненатуральное число");
+        else
+        {
+            if (number <= 0)
+                Console.WriteLine("Задайте число больше 0");
+            else
+                return number;
+        }
+    }
+}
+
+int SumElements(int m, int n)
+{
+    if (n == m) return m;
+    else return n + SumElements(m, n - 1);
+}
+
+
+int m = GetNumber("Введите натуральное число M");
+int n = GetNumber("Введите натуральное число N");
+if (n > m)
+{
+    Console.WriteLine($"Cумма натуральных элементов в промежутке от {m} до {n} равна:{SumElements(m, n)}");
+}
+else
+{
+    Console.WriteLine($"Cумма натуральных элементов в промежутке от {n} до {m} равна:{SumElements(n, m)}");
+}
+
+
---
+++ task68/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29
+*/
+
+
+int GetNumber(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        string num = Console.ReadLine();
+        if (int.TryParse(num, out int number) == false)
+            Console.WriteLine("Введено ненатуральное число");
+        else
+        {
+            if (number < 0)
+                Console.WriteLine("Задайте число больше 0");
+            else
+                return number;
+        }
+    }
+}
+
+int FunctionAckerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAckerman(m - 1, 1);
+    else return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
+}
+
+int m = GetNumber("Введите неотрицательное число m");
+int n = GetNumber("Введите неотрицательное число n");
+Console.WriteLine($"Функции Аккермана А({m},{n})={FunctionAckerman(m, n)}");
+
+
